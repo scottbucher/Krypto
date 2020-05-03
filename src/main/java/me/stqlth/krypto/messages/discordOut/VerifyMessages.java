38556d78d6decf63949b63a8346172cbb7f71b26
Kevin Novak <kevinmnovak@yahoo.com>
2020-05-03 02:39:10 -0400

    Separate roles into different fields

--- src/main/java/me/stqlth/krypto/messages/discordOut/VerifyMessages.java
+++ src/main/java/me/stqlth/krypto/messages/discordOut/VerifyMessages.java
@@ -9,7 +9,7 @@ import java.awt.*;
 
 public class VerifyMessages {
 
-        public void roleVerifySetupMessage(CommandEvent event, TextChannel rolecall) {
+        public void roleVerifySetupMessage(CommandEvent event, TextChannel roleCall) {
             EmbedBuilder builder = new EmbedBuilder();
             SelfUser bot = event.getJDA().getSelfUser();
             String botIcon = bot.getAvatarUrl();
@@ -18,10 +18,11 @@ public class VerifyMessages {
                     .setColor(Color.decode("#00ff80"))
                     .addField("Use this to obtain your roles.", "React with emotes to the corresponding activities you partake in", false)
                     .setFooter("To leave a role, simply remove your reaction of the corresponding role.", botIcon)
-                    .addField("😂 Memes\n🎮 Games\n🎵 Music\n💢 Anime\n🐱 Animals\n🛑 NNSFW\n🎭 Roleplay\n\n⛏ Minecraft\n🍂 Animal Crossing\n🥚 Pokemon\n⚔ Runescape", "", false)
+                    .addField("Topics", "😂 Memes\n🎮 Games\n🎵 Music\n💢 Anime\n🐱 Animals\n🛑 NNSFW\n🎭 Roleplay", false)
+                    .addField("Games", "⛏ Minecraft\n🍂 Animal Crossing\n🥚 Pokemon\n⚔ Runescape", false)
                     .addField("Role Selection", "If there is not a role category that you enjoy contact Stqlth#0001 to request it.", false);
 
-            rolecall.sendMessage(builder.build()).queue(result -> {
+            roleCall.sendMessage(builder.build()).queue(result -> {
                 result.addReaction("😂").queue(); // Memes
                 result.addReaction("🎮").queue(); // Games
                 result.addReaction("🎵").queue(); // Music

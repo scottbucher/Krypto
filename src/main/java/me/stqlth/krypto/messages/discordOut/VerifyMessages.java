a2aaba899d429eb4efa0034e85dadfca31f9aa87
Kevin Novak <kevinmnovak@yahoo.com>
2020-05-03 13:42:07 -0400

    Fix spacing

--- src/main/java/me/stqlth/krypto/messages/discordOut/VerifyMessages.java
+++ src/main/java/me/stqlth/krypto/messages/discordOut/VerifyMessages.java
@@ -8,35 +8,34 @@ import net.dv8tion.jda.api.entities.TextChannel;
 import java.awt.*;
 
 public class VerifyMessages {
+    public void roleVerifySetupMessage(CommandEvent event, TextChannel roleCall) {
+        EmbedBuilder builder = new EmbedBuilder();
+        SelfUser bot = event.getJDA().getSelfUser();
+        String botIcon = bot.getAvatarUrl();
 
-        public void roleVerifySetupMessage(CommandEvent event, TextChannel roleCall) {
-            EmbedBuilder builder = new EmbedBuilder();
-            SelfUser bot = event.getJDA().getSelfUser();
-            String botIcon = bot.getAvatarUrl();
+        builder.setTitle("Role Manager")
+            .setColor(Color.decode("#00ff80"))
+            .addField("Use this to obtain your roles.", "React with emotes to the corresponding activities you partake in", false)
+            .setFooter("To leave a role, simply remove your reaction of the corresponding role.", botIcon)
+            .addField("Topics", "😂 Memes\n🎮 Games\n🎵 Music\n💢 Anime\n🐱 Animals\n🛑 NNSFW\n🎭 Roleplay", false)
+            .addField("Games", "⛏ Minecraft\n🍂 Animal Crossing\n🥚 Pokemon\n⚔ Runescape", false)
+            .addField("Events", "🍿 Movie Streams\n👾 Game Streams", false)
+            .addField("Role Selection", "If there is not a role category that you enjoy contact Stqlth#0001 to request it.", false);
 
-            builder.setTitle("Role Manager")
-                    .setColor(Color.decode("#00ff80"))
-                    .addField("Use this to obtain your roles.", "React with emotes to the corresponding activities you partake in", false)
-                    .setFooter("To leave a role, simply remove your reaction of the corresponding role.", botIcon)
-                    .addField("Topics", "😂 Memes\n🎮 Games\n🎵 Music\n💢 Anime\n🐱 Animals\n🛑 NNSFW\n🎭 Roleplay", false)
-                    .addField("Games", "⛏ Minecraft\n🍂 Animal Crossing\n🥚 Pokemon\n⚔ Runescape", false)
-                    .addField("Events", "🍿 Movie Streams\n👾 Game Streams", false)
-                    .addField("Role Selection", "If there is not a role category that you enjoy contact Stqlth#0001 to request it.", false);
-
-            roleCall.sendMessage(builder.build()).queue(result -> {
-                result.addReaction("😂").queue(); // Memes
-                result.addReaction("🎮").queue(); // Games
-                result.addReaction("🎵").queue(); // Music
-                result.addReaction("💢").queue(); // Anime
-                result.addReaction("🐱").queue(); // Animals
-                result.addReaction("🛑").queue(); // NNSFW
-                result.addReaction("🎭").queue(); // Roleplay
-                result.addReaction("⛏").queue(); // Minecraft
-                result.addReaction("🍂").queue(); // Animal Crossing
-                result.addReaction("🥚").queue(); // Pokemon
-                result.addReaction("⚔").queue(); // Runescape
-                result.addReaction("🍿").queue(); // Movie Streams
-                result.addReaction("👾").queue(); // Game Streams
-            });
-        }
+        roleCall.sendMessage(builder.build()).queue(result -> {
+            result.addReaction("😂").queue(); // Memes
+            result.addReaction("🎮").queue(); // Games
+            result.addReaction("🎵").queue(); // Music
+            result.addReaction("💢").queue(); // Anime
+            result.addReaction("🐱").queue(); // Animals
+            result.addReaction("🛑").queue(); // NNSFW
+            result.addReaction("🎭").queue(); // Roleplay
+            result.addReaction("⛏").queue(); // Minecraft
+            result.addReaction("🍂").queue(); // Animal Crossing
+            result.addReaction("🥚").queue(); // Pokemon
+            result.addReaction("⚔").queue(); // Runescape
+            result.addReaction("🍿").queue(); // Movie Streams
+            result.addReaction("👾").queue(); // Game Streams
+        });
+    }
 }
